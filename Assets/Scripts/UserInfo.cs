84ea7a0867bcb59457fe6939427cc94ce8f66914
bj961 <73685098+bj961@users.noreply.github.com>
2024-06-18 02:47:10 +0900

    Asset Import & Create UI

---
+++ Assets/Scripts/UserInfo.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class UserInfo
+{
+    public static UserInfo myInfo { get => GameManager.Instance.userInfo; }
+
+    public int mineLevel;
+    public double
+    
+
+
+}
